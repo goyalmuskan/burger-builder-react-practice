945755af202079809be864cd314e1877d3448bf1
Muskan Goyal <47412347+goyalmuskan@users.noreply.github.com>
2020-10-14 19:51:51 +0530

    CSS Fixes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 
-const layout = (props) => (
-    <Aux>
-        <div> Toolbar, SideDrawer, BackDrop </div>
+const layout = (props) => {
+    const style = {
+        'textAlign': 'center',
+        'padding': '10px'
+    }
+
+    return (<Aux>
+        <div style={style}> Toolbar, SideDrawer, BackDrop </div>
         <main className={classes.content}>
             {props.children}
         </main>
-    </Aux>
-);
+    </Aux>)
+};
 
 export default layout;
