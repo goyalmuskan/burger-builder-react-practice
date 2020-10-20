72116689e3af5e770dd29b95f81439261098fb68
Muskan Goyal <47412347+goyalmuskan@users.noreply.github.com>
2020-10-20 21:55:58 +0530

    Improved Folder Structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 // import classes from '../../UI/Button/Button.module.css';
 
-const orderSummary = (props) => {
-    const ingredientSummary =  Object.keys(props.ingredients).map( ingredient => {
-        return <li key={ingredient}> <span style={{textTransform: 'capitalize'}}> {ingredient}</span>: {props.ingredients[ingredient]} </li>;
-    })
-    return (
-        <Aux>
-            <h3> Your Order Summary :</h3>
-            <p> Your customised delicious burger has the following chosen ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p> Continue to Checkout? </p> 
-            <p> Total: <strong>Rs. {props.price}/-</strong> </p>
-            <Button clicked={props.cancel} buttonType="Danger"> CANCEL </Button>
-            <Button buttonType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    )
-};
+class OrderSummary extends Component {
 
-export default orderSummary;
+    // this can be changed back to a functional component
+    componentDidUpdate() {
+        console.log('[OrderSummary] will Update');
+    }
+
+    render() {
+        const ingredientSummary =  Object.keys(this.props.ingredients).map( ingredient => {
+            return <li key={ingredient}> <span style={{textTransform: 'capitalize'}}> {ingredient}</span>: {this.props.ingredients[ingredient]} </li>;
+        })
+ 
+        return (
+            <Aux>
+                <h3> Your Order Summary :</h3>
+                <p> Your customised delicious burger has the following chosen ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p> Continue to Checkout? </p> 
+                <p> Total: <strong>Rs. {this.props.price}/-</strong> </p>
+                <Button clicked={this.props.cancel} buttonType="Danger"> CANCEL </Button>
+                <Button buttonType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        )
+    }   
+}
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import OrderSummary from '../../Burger/OrderSummary/OrderSummary';
 import Backdrop from '../Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
-const modal = (props) => (
-    <Aux>
-        <div style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-            }} 
-            className={classes.Modal}>
-            {props.children}
-            <OrderSummary ingredients={props.ingredients} cancel={props.modalClosed} purchaseContinue={props.purchaseContinue} price={props.price}/>
-        </div>
-        <Backdrop show={props.show} clicked={props.modalClosed}  />
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState){
+        if(nextProps.show !== this.props.show) {
+            return true;
+        }
+        return false;
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal] will Update');
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                    }} 
+                    className={classes.Modal}>
+                    {this.props.children}
+                    <OrderSummary ingredients={this.props.ingredients} cancel={this.props.modalClosed} purchaseContinue={this.props.purchaseContinue} price={this.props.price}/>
+                </div>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}  />
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
